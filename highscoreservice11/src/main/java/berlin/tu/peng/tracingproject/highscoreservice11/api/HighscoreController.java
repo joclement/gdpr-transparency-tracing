915b790736f36edd2d0b68239e9f738f8644a6ba
Juri Welz <juriwelz@gmail.com>
2020-06-22 16:30:41 +0200

    [Bugfix] highscores service now reachable on port 8082

--- highscoreservice11/src/main/java/berlin/tu/peng/tracingproject/highscoreservice11/api/HighscoreController.java
+++ highscoreservice11/src/main/java/berlin/tu/peng/tracingproject/highscoreservice11/api/HighscoreController.java
@@ -2,11 +2,13 @@ package berlin.tu.peng.tracingproject.highscoreservice11.api;
 
 import berlin.tu.peng.tracingproject.highscoreservice11.service.HighscoreService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@RestController
 public class HighscoreController {
 
     private HighscoreService highscoreService;
