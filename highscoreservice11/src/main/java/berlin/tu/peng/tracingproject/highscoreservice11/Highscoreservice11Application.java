04e366c476c52cc19f483e38121e71be962dcd6d
Joris Clement <j.clement@campus.tu-berlin.de>
2020-06-29 09:19:57 +0200

    Add tracing to highscore service

--- highscoreservice11/src/main/java/berlin/tu/peng/tracingproject/highscoreservice11/Highscoreservice11Application.java
+++ highscoreservice11/src/main/java/berlin/tu/peng/tracingproject/highscoreservice11/Highscoreservice11Application.java
@@ -1,7 +1,14 @@
 package berlin.tu.peng.tracingproject.highscoreservice11;
 
+import io.jaegertracing.Configuration;
+import io.jaegertracing.Configuration.ReporterConfiguration;
+import io.jaegertracing.Configuration.SamplerConfiguration;
+import io.jaegertracing.internal.JaegerTracer;
+import io.jaegertracing.internal.samplers.ConstSampler;
+import io.opentracing.util.GlobalTracer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Highscoreservice11Application {
@@ -10,4 +17,19 @@ public class Highscoreservice11Application {
 		SpringApplication.run(Highscoreservice11Application.class, args);
 	}
 
+	@Bean
+	public static JaegerTracer getTracer() {
+		SamplerConfiguration samplerConfig = SamplerConfiguration.fromEnv()
+				.withType(ConstSampler.TYPE)
+				.withParam(1);
+
+		ReporterConfiguration reporterConfig = ReporterConfiguration.fromEnv()
+				.withLogSpans(true);
+
+		Configuration config = new Configuration("highscore")
+				.withSampler(samplerConfig)
+				.withReporter(reporterConfig);
+
+		return config.getTracer();
+	}
 }
