e51b5418d410e052458928a5e09053dace19c4bd
Joris Clement <j.clement@campus.tu-berlin.de>
2020-06-29 17:56:29 +0200

    Add tracing to hello world service

--- helloworldservice11/src/main/java/berlin/tu/peng/tracingproject/helloworldservice11/api/HellorworldController.java
+++ helloworldservice11/src/main/java/berlin/tu/peng/tracingproject/helloworldservice11/api/HellorworldController.java
@@ -1,13 +1,24 @@
 package berlin.tu.peng.tracingproject.helloworldservice11.api;
 
+import io.opentracing.Span;
+import io.opentracing.Tracer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HellorworldController {
 
+    private Tracer tracer;
+
+    public HellorworldController(Tracer tracer) {
+        this.tracer = tracer;
+    }
+
     @GetMapping(path = "/helloworld")
     public String sayHello(){
+        Span span = tracer.buildSpan("hello world").start();
+        span.setTag("purpose", "fun");
+        span.finish();
         return "Hello World";
     }
 }
