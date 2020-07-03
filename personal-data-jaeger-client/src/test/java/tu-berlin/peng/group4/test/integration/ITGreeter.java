e1d39daf0df417bf1f180062ce73057c8c64f9bd
Joris Clement <j.clement@campus.tu-berlin.de>
2020-07-03 10:29:25 +0200

    Add initial maven library template
    Template got from:
    https://github.com/Bernardo-MG/library-maven-archetype

---
+++ personal-data-jaeger-client/src/test/java/tu-berlin/peng/group4/test/integration/ITGreeter.java
@@ -0,0 +1,60 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2020 the original author or authors.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package tu-berlin.peng.group4.test.integration;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+import tu-berlin.peng.group4.Greeter;
+
+/**
+ * Integration tests for {@link Greeter}.
+ * <p>
+ * This is actually a placeholder test for the integration tests suite.
+ *
+ * @author Joris Clement
+ *
+ */
+@RunWith(JUnitPlatform.class)
+public final class ITGreeter {
+
+    /**
+     * Default constructor.
+     */
+    public ITGreeter() {
+        super();
+    }
+
+    /**
+     * Tests that the {@code Greeter} returns a greeting.
+     */
+    @Test
+    public final void sayHello() {
+        Assertions.assertEquals("Hello World!", new Greeter().sayHello());
+    }
+
+}
