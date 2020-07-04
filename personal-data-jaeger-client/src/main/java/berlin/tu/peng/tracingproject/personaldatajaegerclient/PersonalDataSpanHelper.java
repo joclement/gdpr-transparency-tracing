3db53ac86983d15c4454cb77835ab5d0dea60588
Joris Clement <j.clement@campus.tu-berlin.de>
2020-07-04 21:35:55 +0200

    Add a personal data span helper, #36
    The other more elegant approach doesn't work. So add a simple helper,
    which will make it easier to set the information about personal data.

---
+++ personal-data-jaeger-client/src/main/java/berlin/tu/peng/tracingproject/personaldatajaegerclient/PersonalDataSpanHelper.java
@@ -0,0 +1,103 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2020 the original author or authors.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package berlin.tu.peng.tracingproject.personaldatajaegerclient;
+
+import io.opentracing.Span;
+import io.opentracing.tag.StringTag;
+import io.opentracing.tag.BooleanTag;
+import io.opentracing.tag.Tag;
+
+import java.util.List;
+
+
+/**
+ * {@link PersonalDataSpan} represents an extension of OpenTracing's Span
+ * regarding the tracing of the processing of personal data.
+ *
+ * @author Joris Clement
+ */
+public class PersonalDataSpanHelper {
+
+    public static final StringTag PURPOSE = new StringTag("purpose");
+
+    public static final StringTag DATA_CATEGORY = new StringTag("category");
+
+    public static final StringTag RECIPIENTS = new StringTag("recipients");
+
+    public static final BooleanTag TRANSFERRED_TO_3RDPARTY = new BooleanTag("3rdparty");
+
+    public static final StringTag STORAGE_DURATION = new StringTag("duration");
+
+    public static final StringTag ORIGIN = new StringTag("origin");
+
+    public static final BooleanTag AUTOMATED = new BooleanTag("auto");
+
+
+    public static void setPurpose(Span span, String purpose) {
+        PURPOSE.set(span, purpose);
+    }
+
+    public static void setRecipients(Span span, List<String> recipients) {
+        RECIPIENTS.set(span, String.join(", ", recipients));
+    }
+
+    public static void setDataCategory(Span span, String dataCategory) {
+        DATA_CATEGORY.set(span, dataCategory);
+    }
+
+    public static void setTransferredTo3rdParty(Span span,
+                                                boolean transferredTo3rdParty) {
+        TRANSFERRED_TO_3RDPARTY.set(span, transferredTo3rdParty);
+    }
+
+    public static void setStorageDuration(Span span, String storageDuration) {
+        STORAGE_DURATION.set(span, storageDuration);
+    }
+
+    public static void setOrigin(Span span, String origin) {
+        ORIGIN.set(span, origin);
+    }
+
+    public static void setAutomated(Span span, boolean automated) {
+        AUTOMATED.set(span, automated);
+    }
+
+    public static void setPersonalInfo(Span span,
+                                       String purpose,
+                                       String dataCategory,
+                                       List<String> recipients,
+                                       boolean transferredTo3rdParty,
+                                       String storageDuration,
+                                       String origin,
+                                       boolean automated) {
+        setPurpose(span, purpose);
+        setDataCategory(span, dataCategory);
+        setRecipients(span, recipients);
+        setTransferredTo3rdParty(span, transferredTo3rdParty);
+        setStorageDuration(span, storageDuration);
+        setOrigin(span, origin);
+        setAutomated(span, automated);
+    }
+}
