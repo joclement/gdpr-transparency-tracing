1602305d683f813cc76e7e1e6c8f8c039ab26e46
Joris Clement <j.clement@campus.tu-berlin.de>
2020-07-09 23:33:29 +0200

    Save how to install NodeJs
    This can be used for readers of this repository to install NodeJs. I
    used to install NodeJS for AWS. Later on that wasn't necessary anymore
    because of the use of a private docker registry.

---
+++ vue-frontend/install-node.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Reference
+# https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-up-node-on-ec2-instance.html
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+node -e "console.log('Running Node.js ' + process.version)"
