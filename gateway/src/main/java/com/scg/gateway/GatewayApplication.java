bfa1b89310c7fe261d0da13dcb855da5a7861886
Juri Welz <juriwelz@gmail.com>
2020-07-13 16:17:30 +0200

    Removed Thymeleaf, forwarding / to /index.html

--- gateway/src/main/java/com/scg/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/scg/gateway/GatewayApplication.java
@@ -1,19 +1,30 @@
 package com.scg.gateway;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import java.net.URI;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 
 @Controller
 @SpringBootApplication
 public class GatewayApplication {
 
-
-
-
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> routerFunction() {
+        return  route(GET("/"), req ->
+                ServerResponse.temporaryRedirect(URI.create("/index.html"))
+                        .build());
     }
 }
