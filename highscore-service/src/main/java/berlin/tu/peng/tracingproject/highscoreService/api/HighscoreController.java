426c9407a9eeb56682911838e516e19ab0713b42
Joris Clement <j.clement@campus.tu-berlin.de>
2020-07-13 23:59:19 +0200

    Fix renaming modules to get correct build
    Some files were lost by the module renaming and other content renaming
    was forgotten. This caused build errors.
    These should all be fixed with this.
    For further details check the previous commit, which prepares the
    gitignore rules for this.
    
    Improve #47.

--- highscore-service/src/main/java/berlin/tu/peng/tracingproject/highscoreService/api/HighscoreController.java
+++ highscore-service/src/main/java/berlin/tu/peng/tracingproject/highscoreService/api/HighscoreController.java
@@ -2,10 +2,10 @@ package berlin.tu.peng.tracingproject.highscoreService.api;
 
 import berlin.tu.peng.tracingproject.highscoreService.model.HighscoreModel;
 import berlin.tu.peng.tracingproject.highscoreService.service.HighscoreService;
-import berlin.tu.peng.tracingproject.personaldatajaegerclient.Category;
+import berlin.tu.peng.tracingproject.transparencyOpentracingHelper.Category;
 import berlin.tu.peng.tracingproject.transparencyOpentracingHelper.PersonalDataSpanHelper;
-import berlin.tu.peng.tracingproject.personaldatajaegerclient.Purpose;
-import berlin.tu.peng.tracingproject.personaldatajaegerclient.StorageDuration;
+import berlin.tu.peng.tracingproject.transparencyOpentracingHelper.Purpose;
+import berlin.tu.peng.tracingproject.transparencyOpentracingHelper.StorageDuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- transparency-opentracing-helper/src/main/java/berlin/tu/peng/tracingproject/transparencyOpentracingHelper/Recipient.java
+++ transparency-opentracing-helper/src/main/java/berlin/tu/peng/tracingproject/transparencyOpentracingHelper/Recipient.java
@@ -1,4 +1,4 @@
-package berlin.tu.peng.tracingproject.personaldatajaegerclient;
+package berlin.tu.peng.tracingproject.transparencyOpentracingHelper;
 
 public enum Recipient {
     DOOFENSCHMIRTZ_EVIL_INC,
